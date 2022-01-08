6a8c4c9d75e86ecca4629849e3b5d9b3da65b347
Jingyu Xiang <jxiang2@ualberta.ca>
2022-01-08 16:50:04 -0700

    add hooks & context

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+// pages and components
+import Dashboard from './pages/Dashboard'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import Create from './pages/Create'
+import ProjectDetail from './pages/ProjectDetail'
+
+// stules
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <div className='container'>
+          <Switch><Route exact path='/'><Dashboard/></Route></Switch>
+          <Switch><Route exact path='/create'><Create/></Route></Switch>
+          <Switch><Route exact path='/signup'><Signup/></Route></Switch>
+          <Switch><Route exact path='/projects/:id'><ProjectDetail/></Route></Switch>
+          <Switch><Route exact path='/login'><Login/></Route></Switch>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/ProjectDetail.jsx
@@ -0,0 +1,9 @@
+import './ProjectDetail.css'
+
+export default function ProjectDetail() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
