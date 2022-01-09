45cf8c19eb39615a7438a5d3aea797fcbc874617
Jingyu Xiang <jxiang2@ualberta.ca>
2022-01-09 15:48:17 -0700

    add category selector

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -1,69 +1,73 @@
-// add, remove items from firestore
-
 import { useReducer, useEffect, useState } from "react"
-import { projectFirestore, timeStamp } from '../firebase/config'
+import { projectFirestore, timestamp } from "../firebase/config"
 
 let initialState = {
-    document: null,
-    isPending: false,
-    error: null,
-    success: null
+  document: null,
+  isPending: false,
+  error: null,
+  success: null
 }
 
 const firestoreReducer = (state, action) => {
-    switch (action.type) {
-        case 'IS_PENDING':
-            return {isPending: true, document: null, success: false, error: null}
-        case 'ADDED_DOCUMENT':
-            return {isPending: false, document: action.payload, success: true, error: null}
-        case "DELETED_DOCUMENT":
-            return {isPending: false, document: null, success: true, error: null}
-        case 'ERROR':
-            return {isPending: false, document: null, success: false, error: action.payload}
-        default:
-            return state
-    }
+  switch (action.type) {
+    case 'IS_PENDING':
+      return { isPending: true, document: null, success: false, error: null }
+    case 'ADDED_DOCUMENT':
+      return { isPending: false, document: action.payload, success: true, error: null }
+    case 'DELETED_DOCUMENT':
+      return { isPending: false, document: null, success: true, error: null }
+    case 'ERROR':
+      return { isPending: false, document: null, success: false, error: action.payload }
+    default:
+      return state
+  }
 }
 
 export const useFirestore = (collection) => {
-    const [response, dispatch] = useReducer(firestoreReducer, initialState)
-    const [isCancelled, setIsCancelled] = useState(false)
+  const [response, dispatch] = useReducer(firestoreReducer, initialState)
+  const [isCancelled, setIsCancelled] = useState(false)
 
-    // collection reference
-    const ref = projectFirestore.collection(collection)
+  // collection ref
+  const ref = projectFirestore.collection(collection)
 
-    // only dispatch is not cancelled
-    const dispatchIfNotCancelled = (action) => {
-        if (!isCancelled) dispatch(action)
+  // only dispatch is not cancelled
+  const dispatchIfNotCancelled = (action) => {
+    if (!isCancelled) {
+      dispatch(action)
     }
+  }
 
-    // add document
-    const addDocument = async (doc) => {
-        dispatch({type: 'IS_PENDING'})
-        try {
-            const createdAt = timeStamp.fromDate(new Date()) // current datetiem
-            const addedDocument = await ref.add({...doc, createdAt})
-            dispatchIfNotCancelled({type: 'ADDED_DOCUMENT', payload: addedDocument})
-        } catch(err) {
-            dispatchIfNotCancelled({type: 'ERROR', payload: err.message})
-        }
+  // add a document
+  const addDocument = async (doc) => {
+    dispatch({ type: 'IS_PENDING' })
+
+    try {
+      const createdAt = timestamp.fromDate(new Date())
+      const addedDocument = await ref.add({ ...doc, createdAt })
+      dispatchIfNotCancelled({ type: 'ADDED_DOCUMENT', payload: addedDocument })
     }
-
-    // delete document
-    const deleteDocument = async (id) => {
-        dispatch({type: 'IS_PENDING'})
-        try {
-            await ref.doc(id).delete()
-            dispatchIfNotCancelled({type: 'DELETED_DOCUMENT'})
-        } catch(err) {
-            dispatchIfNotCancelled({type: 'ERROR', payload: err.message})
-        }
+    catch (err) {
+      dispatchIfNotCancelled({ type: 'ERROR', payload: err.message })
     }
+  }
 
-    // clearup function
-    useEffect(()=>{
-        return () => setIsCancelled(true)
-    }, [])
+  // delete a document
+  const deleteDocument = async (id) => {
+    dispatch({ type: 'IS_PENDING' })
 
-    return {addDocument, deleteDocument, response}
+    try {
+      await ref.doc(id).delete()
+      dispatchIfNotCancelled({ type: 'DELETED_DOCUMENT' })
+    }
+    catch (err) {
+      dispatchIfNotCancelled({ type: 'ERROR', payload: 'could not delete' })
+    }
+  }
+
+  useEffect(() => {
+    return () => setIsCancelled(true)
+  }, [])
+
+  return { addDocument, deleteDocument, response }
+
 }
--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,6 +1,12 @@
-// styles
 import Select from 'react-select'
-import { useState } from 'react'
+import { useHistory } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { useCollection } from '../hooks/useCollection'
+import { useAuthContext } from '../hooks/useAuthContext' 
+import { useFirestore } from '../hooks/useFirestore'
+import { timestamp } from '../firebase/config'
+
+// styles
 import './Create.css'
 
 const categories = [
@@ -12,20 +18,74 @@ const categories = [
 
 export default function Create() {
 
+    const { addDocument, response } = useFirestore('projects')
+    const { user: authUser } = useAuthContext()
+    const { documents } = useCollection('users')
+
+    const history = useHistory()
+    const [users, setUsers] = useState([])
     const [name, setName] = useState('')
     const [detail, setDetail] = useState('')
     const [date, setDate] = useState('')
     const [category, setCategory] = useState('')
-    // const [asignedUsers, setAssignedUsers] = useState([])
+    const [asignedUsers, setAssignedUsers] = useState([])
+    const [formError, setFormError] = useState(null)
 
-    const handleSubmit = (e) => {
+    useEffect(()=>{
+        if (documents) {
+            const options = documents.map((user)=> {
+                return {value: user, label: user.displayName}
+            })
+            setUsers(options)
+        }
+    }, [documents])
+
+    const handleSubmit = async (e) => {
         e.preventDefault()
-        console.log(name, detail, date, category)
+        setFormError(null)
+    
+        if (!category) {
+          setFormError('Please select a project category.')
+          return
+        }
+        if (asignedUsers.length < 1) {
+          setFormError('Please assign the project to at least 1 user')
+          return
+        }
+
+        const createdBy = { 
+            displayName: authUser.displayName, 
+            photoURL: authUser.photoURL,
+            id: authUser.uid
+        }
+    
+        const assignedUsersList = asignedUsers.map(u => {
+          return { 
+            displayName: u.value.displayName, 
+            photoURL: u.value.photoURL,
+            id: u.value.id
+          }}
+        )
+    
+        const project = {
+          name,
+          detail,
+          assignedUsersList, 
+          createdBy,
+          category: category.value,
+          date: timestamp.fromDate(new Date(date)),
+          comments: []
+        }
+    
+        await addDocument(project)
+        if (!response.error) {
+          history.push('/')
+        }
     }
 
     return (
         <div className='create-form'>
-            <h2 className='page-title'>Schedule New Event</h2>
+            <h2 className='page-title'>Schedule New Project</h2>
             <form onSubmit={handleSubmit}>
                 <label>
                     <span>Project Name</span>
@@ -59,7 +119,7 @@ export default function Create() {
                 <label>
                     <span>Set Category</span>
                     <Select
-                     maxMenuHeight='80px'
+                     maxMenuHeight='100px'
                      onChange={(option)=>setCategory(option)}
                      options={categories}
                     />
@@ -67,10 +127,15 @@ export default function Create() {
 
                 <label>
                     <span>Set Assignee</span>
-                    {/* {set assignee} */}
+                    <Select 
+                     maxMenuHeight='100px'
+                     options={users}
+                     onChange={(option)=>setAssignedUsers(option)}
+                     isMulti/>
                 </label>
 
-                <button className='btn'>Add Event</button>
+                {formError && <p className='error'>{formError}</p>}
+                <button style={{'marginBottom': '100px'}} className='btn'>Add Project</button>
             </form>
         </div>
     )
