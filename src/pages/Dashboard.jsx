b43032fae7153979a04a7131559f6277d7a24633
Jingyu Xiang <jxiang2@ualberta.ca>
2022-01-10 11:59:02 -0700

    style filter bar

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { useCollection } from '../hooks/useCollection'
 import ProjectList from '../components/ProjectList/ProjectList'
 import ProjectFilter from '../components/ProjectFilter/ProjectFilter'
@@ -8,12 +9,17 @@ import './Dashboard.css'
 export default function Dashboard() {
 
     const { documents, error } = useCollection('projects')
+    const [currentFilter, setCurrentFilter] = useState('all')
+
+    const changeFilter = (newFilter) => {
+        setCurrentFilter(newFilter)
+    }
 
     return (
         <div>
             <h3 className='page-title'>Dashboard</h3>
             {error && <p className='error'>{error}</p>}
-            {documents && <ProjectFilter/>}
+            {documents && <ProjectFilter currentFilter={currentFilter} changeFilter={changeFilter}/>}
             {documents && <ProjectList projects={documents}/>}
         </div>
     )
