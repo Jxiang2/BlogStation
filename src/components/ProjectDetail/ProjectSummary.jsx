08337abf36d2de4f4367cf1797052b0f0e10123d
Jingyu Xiang <jxiang2@ualberta.ca>
2022-01-10 11:29:10 -0700

    mark as complete done

--- src/components/ProjectDetail/ProjectSummary.jsx
+++ src/components/ProjectDetail/ProjectSummary.jsx
@@ -1,35 +1,42 @@
 import { useFirestore } from '../../hooks/useFirestore'
 import { useAuthContext } from '../../hooks/useAuthContext'
 import Avatar from '../Avatar/Avatar'
+import { useHistory } from 'react-router-dom'
 
 export default function ProjectSummary( { project }) {
 
     const { deleteDocument } = useFirestore('projects')
     const { user: authUser} = useAuthContext()
+    const history = useHistory()
 
     const handleClick = (e) => {
         deleteDocument(project.id)
+        history.push('/')
     }
 
     return (
-        <div className="project-summary">
-            <h2 className="page-title">{project.name}</h2>
-            <p>By {project.createdBy.displayName}</p>
-            <p className="date">Project due by {project.date.toDate().toDateString()}</p>
+        <div>
+            <div className="project-summary">
+                <h2 className="page-title">{project.name}</h2>
 
-            <p className="detail">{project.detail}</p>
+                <p>By {project.createdBy.displayName}</p>
 
-            <h4>Project assigned to:</h4>
+                <p className="date">Project due by {project.date.toDate().toDateString()}</p>
 
-            <div className='assigned-users'>
-                {project.assignedUsersList.map(user=>(
-                <div key={user.id}>
-                    <Avatar src={user.photoURL}/>
-                </div>))}
+                <p className="detail">{project.detail}</p>
+
+                <h4>Project assigned to:</h4>
+
+                <div className='assigned-users'>
+                    {project.assignedUsersList.map(user=>(
+                    <div key={user.id}>
+                        <Avatar src={user.photoURL}/>
+                    </div>))}
+                </div>
             </div>
-            
             {authUser.uid === project.createdBy.id 
-            && <button className='btn' onClick={handleClick}>Mark as completed</button>}
+                && <button className='btn' style={{'marginBottom':'20px'}} onClick={handleClick}>Mark as completed</button>}
         </div>
+        
     )
 }
