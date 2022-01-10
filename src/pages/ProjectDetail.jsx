03efc5b1e3a542f608dd5890995b26eb45404e1b
Jingyu Xiang <jxiang2@ualberta.ca>
2022-01-09 20:39:46 -0700

    reformat

--- src/pages/ProjectDetail.jsx
+++ src/pages/ProjectDetail.jsx
@@ -1,5 +1,5 @@
 import { useParams } from 'react-router-dom'
-import ProjectSummary from './ProjectSummary'
+import ProjectSummary from '../components/ProjectSummary'
 import { useDocument } from '../hooks/useDocument'
 
 // styles
